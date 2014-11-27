11a1e2b08e0ac2c41d6a45484c06376640a7c70a
Arun Rangarajan <arangarajan@college.harvard.edu>
2014-11-27 15:30:17 -0500

    Final Blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "Arun", password: "Redsox04", except: [:index, :show]
+
+	def index
+	@articles = Article.all
+	end
+
 	def new
 		@article = Article.new
 	end
@@ -33,6 +39,13 @@ class ArticlesController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'articles/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'articles#index'
 
 
 
